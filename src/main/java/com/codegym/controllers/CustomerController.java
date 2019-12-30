8b689b8c599ebd1d4ffb2d6006e7891675202f89
nghiemducnam <nghiemducnam0312@gmail.com>
2019-12-30 22:00:11 +0700

    9:58-get an info customer's and edit it

--- src/main/java/com/codegym/controllers/CustomerController.java
+++ src/main/java/com/codegym/controllers/CustomerController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
@@ -18,6 +15,8 @@ import java.util.List;
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+
+//    -----------------------get customers list--------------------------
     @RequestMapping(value = "/user/customer/", method = RequestMethod.GET)
     public ResponseEntity<List<Customer>> listCustomer(){
         List<Customer> customers = customerService.findAll();
@@ -27,6 +26,7 @@ public class CustomerController {
         return new ResponseEntity<List<Customer>>(customers, HttpStatus.OK);
     }
 
+//    -------------------------create a new customer--------------------------
     @RequestMapping(value = "/admin/customer/", method = RequestMethod.POST)
     public ResponseEntity<Void>  createCustomer(
             @RequestBody Customer customer, UriComponentsBuilder uriComponentsBuilder){
@@ -36,6 +36,32 @@ public class CustomerController {
             ).buildAndExpand(customer.getId()).toUri());
             return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
 
+    }
+
+//    ------------------------------get an infomation customer----------------------
+
+    @RequestMapping(value = "/admin/customer/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") Long id){
+        Customer customer = customerService.findById(id);
+        if(customer == null){
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+        return  new ResponseEntity<Customer>(customer, HttpStatus.OK);
+
+    }
+//    ----------------------------Edit a customer---------------------------------
+    @RequestMapping(value = "/admin/customer/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Customer> editCustomer(@PathVariable("id") Long id, @RequestBody Customer customer){
+        Customer currentCustomer = customerService.findById(id);
+        if(currentCustomer == null){
+            return  new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+        currentCustomer.setFirstName(customer.getFirstName());
+        currentCustomer.setLastName(customer.getLastName());
+        currentCustomer.setId(customer.getId());
+
+        customerService.save(currentCustomer);
+        return new ResponseEntity<Customer>(currentCustomer, HttpStatus.OK);
     }
 
 
